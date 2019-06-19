558c297d24e68ef910e223677af1cc7964cfa7f4
2BSJ <h3tmdwns@gmail.com>
2019-06-19 16:40:34 +0900

    Initial commit

---
+++ variable.py
@@ -0,0 +1,24 @@
+# 변수 이름은 문자, 숫자, _로 구성된다.
+import keyword
+
+friend = 1
+a = 10
+my_name = '양승준'
+myName = '양승준'
+_yourname = '둘리'
+member1 = '도우넛'
+
+# 에러
+# friends$ = 1
+# a! = 10
+# 123a = 10
+#def = 10
+
+#from keyword import kwlist
+print(keyword.kwlist)
+
+#한글이름의 변수도 사용이 가능하당.
+
+가격 = '10000원'
+
+print(가격)
